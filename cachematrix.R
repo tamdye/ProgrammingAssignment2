f0753c4a4cae706eb7d05f9ccb688634192223a0
tamdye <tamdye@outlook.com>
2015-02-22 10:01:15 -0800

    edits

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,8 @@
 ## This function creates a matrix object that can cache its inverse.
 ## This function only works for a square matrix.
 
-makeMatrix <- function(x = matrix()) {
-        ## makeMatrix is a function that takes a matrix as input.  The
+makeCacheMatrix <- function(x = matrix()) {
+        ## makeCacheMatrix is a function that takes a matrix as input.  The
         ## input matrix must be square in order to calculate an inverse
         ## with this function.
         d <- dim(x)
@@ -35,27 +35,27 @@ makeMatrix <- function(x = matrix()) {
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
-        ## the makeMatrix function returns 4 other functions
+        ## the makeCacheMatrix function returns 4 other functions
 }
 
 
 
 ## This function computes the inverse of the matrix object returned by
-## makeMatrix. If the inverse has already been calculated and
+## makeCacheMatrix. If the inverse has already been calculated and
 ## the matrix has not changed, then cacheSolve should retrieve the inverse
 ## from the cache.  If the inverse has not been calculated or the matrix
 ## has changed, then cacheSolve should calculate the inverse.
 
 cacheSolve <- function(x=matrix(), ...) {
         ## cacheSolve is a function that takes a matrix x as input.
-        ## The input matrix x was created with makeMatrix and also carries
+        ## The input matrix x was created with makeCacheMatrix and also carries
         ## four function objects with it: set, get, setinverse, and
         ## getinverse.  I needed to specify that the input
         ## to this function was a matrix otherwise the function defaulted
         ## x to a vector and I got an error when trying to call the objects
         ## of x using the $ operator.
         i <- x$getinverse()
-        ## within cacheSolve, set i to be i from makeMatrix, if it exists
+        ## within cacheSolve, set i to be i from makeCacheMatrix, if it exists
         if(!is.null(i)) {
                 message("getting cached data")
                 return(i)
@@ -65,7 +65,7 @@ cacheSolve <- function(x=matrix(), ...) {
         ## If i doesn't exist, because it is the first time through or the
         ## matrix has changed, then the following four lines are evaluated.
         data <- x$get()
-        ## data is assigned the matrix x from makeMatrix
+        ## data is assigned the matrix x from makeCacheMatrix
         i <- solve(data, ...)
         ## i becomes the inverse of x
         x$setinverse(i)
